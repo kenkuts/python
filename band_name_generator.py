528ad63d0c4cc5d0ffc8e63ca43ea02603809706
Kenneth Kutschera <kennethkutschera@Mac.net>
2025-12-13 23:20:52 +0300

    Band Name Generator

---
+++ band_name_generator.py
@@ -0,0 +1,9 @@
+print("Welcome to the band name generator!")
+print("What's the name of the city you grew up in?")
+city = input()
+
+print("What's your pet's name?")
+pet_name = input()
+
+print("Your band name could be " + city + " " + pet_name)
+
