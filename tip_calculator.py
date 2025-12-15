05af21c90f51c763c39d638a3cb5b6ce78367a6e
Kenneth Kutschera <kennethkutschera@Mac.net>
2025-12-15 23:49:21 +0300

    commit again

--- tip_calculator.py
+++ tip_calculator.py
@@ -1,4 +1,4 @@
-print("Welcome to to the tip calculator!")
+print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill?\n$"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15?\n"))
 split = int(input ("How many people to split the bill?\n"))
