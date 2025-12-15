c9acc93fb56ee08ae64b020374006ed762b13fdc
Kenneth Kutschera <kennethkutschera@Mac.net>
2025-12-15 23:07:19 +0300

    More python

--- task.py
+++ task.py
@@ -1,9 +1,1 @@
-print("Welcome to the band name generator!")
-print("What's the name of the city you grew up in?")
-city = input()
-
-print("What's your pet's name?")
-pet_name = input()
-
-print("Your band name could be " + city + " " + pet_name)
-
+print ("Number of letters in your name: " + str(len(input("Enter your name:\n "))))
